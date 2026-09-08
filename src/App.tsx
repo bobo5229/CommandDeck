2ca5c4c3f88d2bfac6d47084006c4cbd7935ace0
BoBo <11bobo229@gmail.com>
2026-09-08 21:54:20 +0800

    Initialize CommandDeck desktop shell

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>CommandDeck</h1>
+      <p>Windows desktop command center</p>
+      <p>Development shell is running.</p>
+    </main>
+  );
+}
+
+export default App;
